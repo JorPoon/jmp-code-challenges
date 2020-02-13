a7f33e70cc573cccc0ac844d5b57675415f80633
JorPoon <jormpoon@gmail.com>
2020-02-13 17:11:10 -0500

    created sumofArray chanllenge file

---
+++ JavaScript/sumOfArray.js
@@ -0,0 +1,7 @@
+//given two arrays and a value, return true if the given value can be found as the sum of any two digits. 
+//each digit is from one of the array
+
+
+const sum = (a, b, v) => {
+    
+}
