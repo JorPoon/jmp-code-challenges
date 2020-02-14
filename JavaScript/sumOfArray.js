39bfd2748c26709fc2d2165992d3d113428fd5ad
JorPoon <jormpoon@gmail.com>
2020-02-13 19:02:07 -0500

    completed today's challenge

--- JavaScript/sumOfArray.js
+++ JavaScript/sumOfArray.js
@@ -3,5 +3,36 @@
 
 
 const sum = (a, b, v) => {
-    
+    let obj = {}
+    let hasValue = false
+
+    for (let i = 0; i < a.length; i++) {
+        if (!(a[i] in obj)) {
+            let difference = 0
+            if ( v > a[i]) {
+                difference = v - a[i]
+            } else {
+                difference = a[i] - v
+            }
+            obj[difference] = difference
+        }
+    }
+    for (let j = 0; j <= b.length; j++) {
+        if(obj[b[j]]) {
+            return true
+        } 
+    } 
+    return hasValue
 }
+
+
+let arr1 = [1,9,43,5,6,7,8]
+let arr2 = [40, 30, 20, 60, 70]
+
+// tests
+console.log(sum(arr1, arr2, 78))
+console.log(sum(arr1, arr2, 61))
+console.log(sum(arr1, arr2, 24))
+console.log(sum(arr1, arr2, 16))
+console.log(sum(arr1, arr2, 11))
+console.log(sum(arr1, arr2, 46))
